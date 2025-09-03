5d82cd59a6ea720277e4c10e661b84bd3cf7b23d
snaramirez872 <seanramirez818@gmail.com>
2025-09-03 13:33:17 -0400

    fixed centering for header text

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -5,10 +5,10 @@ export default function Games() {
         <div
             className="flex flex-col justify-center items-center py-[5vh] px-[6vh] text-[18px] gap-[2vh]"
         >
-            <h2 className="text-2xl font-semibold mt-[calc(18px*3)]">The Games: Original Releases</h2>
+            <h2 className="text-2xl font-semibold mt-[calc(18px*3)] text-center">The Games: Original Releases</h2>
             <OriginalGames />
-            <h2 className="text-2xl font-semibold mt-[calc(18px*3)]">The Games: Remastered Titles</h2>
-            <h2 className="text-2xl font-semibold mt-[calc(18px*3)]">The Games: Collections</h2>
+            <h2 className="text-2xl font-semibold mt-[calc(18px*3)] text-center">The Games: Remastered Titles</h2>
+            <h2 className="text-2xl font-semibold mt-[calc(18px*3)] text-center">The Games: Collections</h2>
         </div>
     );
 }
