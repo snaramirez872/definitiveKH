f5eb911127ad3d487001af4233b923ca40f0b291
snaramirez872 <seanramirez818@gmail.com>
2025-09-04 16:32:43 -0400

    added collection title map

---
+++ src/lib/collTitles.ts
@@ -0,0 +1,9 @@
+export const collGameTitles: string[] = [
+    "Kingdom Hearts 1.5 HD ReMIX",
+    "Kingdom Hearts 2.5 HD ReMIX",
+    "Kingdom Hearts HD 2.8 Final Chapter Prologue",
+    "Kingdom Hearts 1.5 + 2.5 HD ReMIX",
+    "Kingdom Hearts - The Story So Far",
+    "Kingdom Hearts All-in-One Package",
+    "Kingdom Hearts Integrum Masterpiece"
+]
